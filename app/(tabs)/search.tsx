34c374cc6ab9f428c1edc0505a5b22d3be34d3ca
Jhon Sebastian Reina Gomez <sebasreina0814@gmail.com>
2026-01-17 22:23:19 -0500

    frontend final

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function Search() {
+  return (
+    <SafeAreaView className="flex-1 bg-background-light items-center justify-center">
+      <Text className="text-text-primary text-xl">Search Page</Text>
+      <Text className="text-text-secondary text-sm mt-2">Coming soon...</Text>
+    </SafeAreaView>
+  );
+}
